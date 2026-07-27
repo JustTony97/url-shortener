3557d688a59b29479a21e8029d90898fbcaa6cb3
JustTony97 <antik9792@gmail.com>
2026-07-27 14:42:31 +0300

    fix

--- internal/repository/url.go
+++ internal/repository/url.go
@@ -1,20 +1,16 @@
 package repository
 
-type UrlRepository struct {
-	cache map[string]string
+type UrlRepository map[string]string
+
+func NewUrlRepository() UrlRepository {
+	return make(UrlRepository)
 }
 
-func NewUrlRepository() *UrlRepository {
-	return &UrlRepository{
-		cache: make(map[string]string),
-	}
+func (r UrlRepository) SetShortedUrl(shortedUrl string, originalUrl string) {
+	r[shortedUrl] = originalUrl
 }
 
-func (r *UrlRepository) SetShortedUrl(shortedUrl string, originalUrl string) {
-	r.cache[shortedUrl] = originalUrl
-}
-
-func (r *UrlRepository) GetOriginalUrl(shortedUrl string) (string, bool) {
-	originalUrl, ok := r.cache[shortedUrl]
+func (r UrlRepository) GetOriginalUrl(shortedUrl string) (string, bool) {
+	originalUrl, ok := r[shortedUrl]
 	return originalUrl, ok
 }
